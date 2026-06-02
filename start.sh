1745ebb4acfd9453f647b4462cc8fa0c53506572
mugua <mugua@yaomaitong.cn>
2026-06-02 15:51:30 +0800

    ``` feat(start.sh): 启动脚本中启用gradio服务
    在docker compose命令中添加了--profile gradio参数，
    以便同时启动gradio服务，完善了应用的服务配置。
    ```

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ set -eu
 # docker rmi mineru:latest
 # docker tag mineru:3.2.1 mineru:latest
 
-# docker compose --profile openai-server --profile api --profile gradio up -d
-docker compose --profile openai-server --profile api up -d
+# docker compose --profile openai-server --profile api up -d
+docker compose --profile openai-server --profile api --profile gradio up -d
+
